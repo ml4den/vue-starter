fa4f7045ffa40c02f0aea92ad418ade71de73b43
ml4den <ml4den@users.noreply.github.com>
2021-09-02 22:08:16 +0100

    added to hello

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 in CodeSandbox!!!" />
+  <HelloWorld msg="Hello Vue 3 in CodeSandbox, Netlify and Cloudflare Pages!" />
 </template>
 
 <script>
